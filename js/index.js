14b3be99836138b1cf62015f4767ea87e35996a2
bnielsen1965 <bnielsen1965@gmail.com>
2021-09-09 19:37:07 -0700

    Updated to enable multiple topic tags.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 UI.ready(init);
 
-
+// initialize page when ready
 function init () {
   let list = UI.elements('icon-select');
   for (let item of list) {
@@ -8,13 +8,14 @@ function init () {
   }
 }
 
+// handle button click event
 function buttonClick (e) {
   let group = e.currentTarget.className.split(' ').reduce((g, c) => g || (/^icon-group-.*$/.test(c) ? c : g), null);
-  console.log(group)
   selectButton(group);
   selectList(group);
 }
 
+// highlight the selected button
 function selectButton (group) {
   let list = UI.elements('icon-select');
   for (let item of list) {
@@ -23,10 +24,21 @@ function selectButton (group) {
   }
 }
 
+// highlight selections in list
 function selectList (group) {
+  clearLiState();
+  let list = UI.elements('icon-span');
+  for (let item of list) if (item.classList.contains(group)) item.parentNode.classList.add('icon-selected');
+}
+
+// set or clear parent li class
+function parentLiState (elem, selected) {
+  if (selected) elem.parentNode.classList.add('icon-selected');
+  else elem.parentNode.classList.remove('icon-selected');
+}
+
+// clear list selections
+function clearLiState () {
   let list = UI.elements('icon-li');
-  for (let item of list) {
-    if (item.classList.contains(group)) item.classList.add('icon-selected');
-    else item.classList.remove('icon-selected');
-  }
+  for (let item of list) item.classList.remove('icon-selected');
 }
